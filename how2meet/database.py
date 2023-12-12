c4b18e5bc144374a4e118fa8f45ff88c59e6b51c
Ben Memberg <benmemberg@gmail.com>
2023-12-11 23:02:47 -0500

    Add sqlite db connection options and helper function

--- how2meet/database.py
+++ how2meet/database.py
@@ -5,9 +5,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN")
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN", "sqlite:///./data.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={ "check_same_thread": False },
+    echo=True
+) # `connect_args` only needed with sqlite
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
