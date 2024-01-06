a5a188f1762c9bd86e26b335317d150228c4b279
Taea <88346289+adrastaea@users.noreply.github.com>
2024-01-06 15:49:51 -0500

    Break navigation out to right drawer

--- how2meet/ui/frontend.py
+++ how2meet/ui/frontend.py
@@ -16,23 +16,31 @@ logger = get_logger(__name__)
 @contextmanager
 def frame(navtitle: str):
     """Custom page frame to share the same styling and behavior across all pages"""
-    # ui.colors(primary='#ffa3df', secondary='#c9a3ff', accent='#c9a3ff', positive='#c9a3ff')
-    with ui.header().classes('justify-between text-white'):
-        ui.label('How2Meet').classes('font-bold')
+    with ui.header().classes('justify-between text-white items-center'):
+        ui.link('How2Meet', '/').classes('no-underline font-bold text-white')
         ui.label(navtitle)
-        with ui.row():
+        ui.button(on_click=lambda: right_drawer.toggle()).props('flat color=white icon=menu')
+
+    with ui.right_drawer().classes('gap-0 m-0 p-0') as right_drawer:
+        with ui.row().classes('w-full gap-0'):
             header_links = {
                 'Home': '/',
                 'Events': '/events',
                 'Settings': '/settings',
             }
             for link_text, link_target in header_links.items():
-                if navtitle != link_text:
-                    ui.link(link_text, link_target).classes(replace='text-white')
+                with ui.card().on("click", lambda link_target=link_target: ui.open(link_target)) \
+                              .classes('w-full no-shadow border-b-[1px] rounded-none hover:bg-gray-200') \
+                              .style('cursor: pointer'):
+                    ui.link(link_text, link_target).classes('w-full text-left no-underline font-bold')
 
-    with ui.footer().classes('justify-start text-white'):
-        ui.link('Donate', 'https://www.buymeacoffee.com/').classes('text-white')
-        ui.link('GitHub', 'https://www.github.com/BenMemberg/how2meet').classes('text-white')
+    with ui.footer().classes('justify-between text-white flex items-center'):
+        with ui.row().classes('flex-grow'):
+            ui.link('Donate', 'https://www.buymeacoffee.com/').classes('text-white mr-4')
+            ui.link('GitHub', 'https://www.github.com/BenMemberg/how2meet').classes('text-white mr-4')
+        # put a settings icon on the far right of the footer
+        ui.button(on_click=lambda: ui.open('/settings')).props('flat color=white icon=settings')
+
     yield
 
 @ui.page("/settings")
