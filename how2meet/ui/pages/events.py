235beaaa7a41b9969c1fd14fdb5e43793063cf87
Taea <88346289+adrastaea@users.noreply.github.com>
2024-01-08 19:45:20 -0500

    display events list

--- how2meet/ui/pages/events.py
+++ how2meet/ui/pages/events.py
@@ -1,46 +1,47 @@
-import logging
-import requests
 import uuid
 from collections import namedtuple
 
 from nicegui import ui, APIRouter
+import httpx
 
 from ..components.frames import frame
+from ...utils.logging import get_file_logger
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger = get_file_logger(__name__)
 
 router = APIRouter(prefix="/events",
                    tags=["events"])
 
 @router.page("/")
-def events(user: str = None):
+async def events(user: str = None):
     """List page for all events"""
-
-    try:
-        events = requests.get(f"http://127.0.0.1:8000/api/events/").json()
-
-    except:
-        events = [
-            {'name': 'Test Event 1'}
-        ]
-    logging.debug(events)
     with frame("Events"):
+        async with httpx.AsyncClient() as client:
+            events = await client.get(f"http://localhost:8000/api/events/", timeout=10)
+        if isinstance(events, httpx.Response):
+            events = events.json()
+        else:
+            events = []
         for event in events:
-            ui.label(event['name'])
-
-        ui.button("Back", on_click=lambda: ui.open("/"))
+            with ui.row().classes("w-full"):
+                with ui.card().classes("w-full"):
+                    ui.label(f"Event ID: {event['id']}")
+                    ui.label(f"Event Name: {event['name']}")
+                    ui.button("Details", on_click=lambda event_id=event['id']: ui.open(f"/events/{event_id}"))
+        with ui.row().classes("w-full justify-center"):
+            ui.button("New Event", on_click=lambda: ui.open(f"/events/new/{uuid.uuid4()}"))
+            ui.button("Back", on_click=lambda: ui.open("/"))
 
 
 @router.page("/{event_id}")
-def event_home(event_id: uuid.UUID):
+def event_home(event_id: str):
     """Detail page for a specific event"""
     with frame("Event Home"):
         ui.label(f"Event ID: {event_id}")
         ui.button("Back", on_click=lambda: ui.open("/events"))
 
 @router.page("/new/{event_id}")
-def new_event(event_id: uuid.UUID):
+def new_event(event_id: str):
     """New event creation page
 
     Args:
@@ -73,5 +74,5 @@ def new_event(event_id: uuid.UUID):
                     ui.button("Add guest", on_click=add_guest)
 
         # TODO: use actual url for post request
-        ui.button("Save", on_click=lambda: requests.post(f"http://localhost:8000/events/{event_id}"))
+        ui.button("Save", on_click=lambda: httpx.post(f"http://localhost:8000/events/{event_id}"))
         ui.button("Back", on_click=lambda: ui.open("/"))
