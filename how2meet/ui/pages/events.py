35b8bf38afa1708c22d5fe98ed4f59b7fe7c8f3c
Taea <88346289+adrastaea@users.noreply.github.com>
2024-01-09 21:24:35 -0500

    rm event_model and use async util funcs

--- how2meet/ui/pages/events.py
+++ how2meet/ui/pages/events.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import uuid
 from collections import namedtuple
 
@@ -53,49 +54,65 @@ async def new_event(event_id: str):
     Returns:
         None
     """
-    from ...db.schemas import EventCreate
-    from ..components.event_model import Event, Guest
 
-    event = Event(event_id)
 
-    def add_guest():
-        guest = Guest(uuid.uuid4())
-        event.add_guest(guest)
-        with ui.row().classes("w-full"):
-            ui.input("Name", on_change=lambda text, guest=guest: setattr(guest, 'name', text.value)).classes("w-1/3")
-            ui.input("Email", on_change=lambda text, guest=guest: setattr(guest, 'email', text.value)).classes("w-1/3")
-            ui.input("Phone Number", on_change=lambda text, guest=guest: setattr(guest, 'phone', text.value)).classes("w-1/3")
+    async def add_guest():
+        invite_uuid = str(uuid.uuid4())
+        # TODO add method to remove guest
+        with ui.card().classes("flex-grow position:relative") as card:
+            async def delete(invite_uuid=invite_uuid):
+                card.delete()
+            with ui.row().classes("flex-grow justify-between items-center"):
+                name_input = ui.input("Name")
+                email_input = ui.input("Email")
+                phone_input = ui.input("Phone Number")
+                ui.button("", icon="delete", color="red", on_click=lambda invite_uuid=invite_uuid: delete(invite_uuid)).classes("w-6 h-6")
+
 
     with frame("New Event"):
-        with ui.column().classes("w-full"):
-            with ui.row().classes("w-1/2"):
-                ui.input("Event Name",
-                         on_change=lambda text: setattr(event, 'name', text.value)).classes("w-full")
-            with ui.row():
-                ui.input("Location", on_change=lambda text: setattr(event, 'location', text.value)).classes("w-full")
-            with ui.row():
-                ui.input("User", on_change=lambda text: setattr(event, 'organizer_name', text.value)).classes("w-1/2")
-                ui.input("Password", password=True, password_toggle_button=True,
-                         on_change=lambda text: setattr(event, 'organizer_password', text.value)).classes("w-1/2")
-            with ui.row():
-                with ui.expansion("Start Time"):
-                    ui.date(on_change=lambda date: event.set_time_attr('start_time', date=date.value))
-                    ui.time(on_change=lambda time: event.set_time_attr('start_time', time=time.value))
-                with ui.expansion("End Time"):
-                    ui.date(on_change=lambda date: event.set_time_attr('end_time', date=date.value))
-                    ui.time(on_change=lambda time: event.set_time_attr('end_time', time=time.value))
-            with ui.row():
-                # TODO: disable end time and time selects if all day is checked
-                ui.checkbox("All Day", on_change=lambda checked: setattr(event, 'all_day', checked.value))
-            with ui.row():
-                ui.input("Description", on_change=lambda text: setattr(event, 'description', text.value)).classes("w-full")
-            with ui.row().classes("w-half"):
-                with ui.column():
-                    ui.button("Add guest", on_click=add_guest)
+        with ui.row().classes("flex-grow justify-between"):
+            event_name_input = ui.input("Event Name")
+            event_location_input = ui.input("Location")
+        with ui.row():
+            user_input = ui.input("User")
+            password_input = ui.input("Password", password=True, password_toggle_button=True)
+        with ui.row():
+            with ui.expansion("Start Time"):
+                start_date_input = ui.date()
+                start_time_input = ui.time()
+            with ui.expansion("End Time"):
+                end_date_input = ui.date()
+                end_time_input = ui.time()
+            all_day_checkbox = ui.checkbox("All Day")
+        with ui.row().classes("w-full"):
+            description_input = ui.textarea("Description").classes("w-full")
+        with ui.row().classes("w-full"):
+            with ui.column().classes("w-full"):
+                add_guest_button = ui.button("Add guest", on_click=add_guest)
 
-        # TODO: use actual url for post request
-        # TODO: use details from page to create event
-        ui.button("Save", on_click=lambda: event.save(f"http://localhost:8000/api/events/"))
-        ui.button("Back", on_click=lambda: ui.open("/"))
-        from pprint import pformat
-        ui.button("Test", on_click=lambda: ui.notify(event.__dict__))
+        async def get_event_json():
+            import json
+            return json.dumps({
+                "id": event_id,
+                "name": event_name_input.value,
+                "created": datetime.now().isoformat(),
+                "location": event_location_input.value,
+                "organizer_name": user_input.value,
+                "organizer_password": password_input.value,
+                "description": description_input.value,
+                "start_time": f"{start_date_input.value}T{start_time_input.value}:00",
+                "end_time": f"{end_date_input.value}T{end_time_input.value}:00",
+                "all_day": all_day_checkbox.value
+            })
+
+        async def post_event():
+            event_json = await get_event_json()
+            async with httpx.AsyncClient() as client:
+                response = await client.post("http://localhost:8000/api/events/", data=event_json)
+                response.raise_for_status()
+            ui.open(f"/events/{event_id}")
+
+        # TODO: Post invites to API
+
+        save_button = ui.button("Save", on_click=post_event)
+        back_button = ui.button("Back", on_click=lambda: ui.open("/"))
