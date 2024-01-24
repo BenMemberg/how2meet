607a1d77cc08acde41e4b4e144ba73f6b8a31a2a
Taea <88346289+adrastaea@users.noreply.github.com>
2024-01-24 18:14:14 -0500

    editor init handles existing event

--- how2meet/ui/components/event_editor.py
+++ how2meet/ui/components/event_editor.py
@@ -4,6 +4,7 @@ import uuid
 from nicegui import ui
 
 from how2meet.utils import APIClient as api
+from how2meet.ui.pages.urls import URL_EVENTS, URL_NEW_EVENT, URL_EVENT_HOME
 
 class InviteEditor:
 
@@ -12,7 +13,8 @@ class InviteEditor:
             self.invite = None
 
         async def load(self):
-            self.invite = await api.get_invite(self.invite_id)
+            # self.invite = await api.get_invite(self.invite_id)
+            pass
 
         async def delete(self):
             self.card.delete()
@@ -43,45 +45,51 @@ class InviteEditor:
 class EventEditor:
 
     def __init__(self, event_id=None):
-        self.event_id = event_id or uuid.uuid4()
-        self.event = None
+        self.event_id = event_id or str(uuid.uuid4())
+        self.event = {}
+        self.invites = []
 
     async def load(self):
         self.event = await api.get_event(self.event_id)
 
     async def save(self):
         event_json_str = await self.model_dump()
+        import logging
+        logging.warning(event_json_str)
         status = await api.create_event(event_json_str)
         if status == 200:
             with ui.dialog(value=True) as dialog, ui.card():
                 ui.label("Event created successfully!")
                 with ui.row().classes("w-full justify-center"):
-                    ui.button("Go to event", on_click=lambda: ui.open(f"/events/home/{self.event_id}"))
+                    ui.button("Go to event", on_click=lambda: ui.open(URL_EVENT_HOME.format(self.event_id)))
                     ui.button("OK", on_click=lambda: dialog.close())
 
-    def add_guest():
+    async def add_guest(self):
         invite_id = uuid.uuid4()
         invite_editor = InviteEditor(invite_id)
-        invite_editor.load()
-        invite_editor.render()
+        self.invites.append(invite_editor)
+        await invite_editor.load()
+        await invite_editor.render()
 
     async def render(self):
+        await self.load()
         with ui.row().classes("flex-grow justify-between"):
-            self.event_name_input = ui.input("Event Name")
-            self.event_location_input = ui.input("Location")
+            self.event_name_input = ui.input("Event Name", value=self.event.get("name", ""))
+            self.event_location_input = ui.input("Location", value=self.event.get("location", ""))
             with ui.row():
-                self.user_input = ui.input("User")
+                self.user_input = ui.input("User", value=self.event.get("organizer_name", ""))
                 self.password_input = ui.input("Password", password=True, password_toggle_button=True)
             with ui.row():
                 with ui.expansion("Start Time"):
-                    self.start_date_input = ui.date(value=datetime.now().date().strftime("%Y-%m-%d"))
-                    self.start_time_input = ui.time(value=datetime.now().time().strftime("%H:%M"))
+
+                    self.start_date_input = ui.date(value=self.event.get("start_time", datetime.now().date()).strftime("%Y-%m-%d"))
+                    self.start_time_input = ui.time(value=self.event.get("start_time", datetime.now().time()).strftime("%H:%M"))
                 with ui.expansion("End Time"):
-                    self.end_date_input = ui.date(value=datetime.now().date().strftime("%Y-%m-%d"))
-                    self.end_time_input = ui.time(value=datetime.now().time().strftime("%H:%M"))
-                self.all_day_checkbox = ui.checkbox("All Day")
+                    self.end_date_input = ui.date(value=self.event.get("end_time", datetime.now().date()).strftime("%Y-%m-%d"))
+                    self.end_time_input = ui.time(value=self.event.get("end_time", datetime.now().time()).strftime("%H:%M"))
+                self.all_day_checkbox = ui.checkbox("All Day", value=self.event.get("all_day", False))
             with ui.row().classes("w-full"):
-                self.description_input = ui.textarea("Description").classes("w-full")
+                self.description_input = ui.textarea("Description", value=self.event.get("description", "")).classes("w-full")
             with ui.row().classes("w-full"):
                 with ui.column().classes("w-full"):
                     self.add_guest_button = ui.button("Add guest", on_click=self.add_guest)
@@ -95,11 +103,11 @@ class EventEditor:
             "id": self.event_id,
             "name": self.event_name_input.value,
             "created": datetime.now().isoformat(),
+            "start_time": f"{self.start_date_input.value}T{self.start_time_input.value}:00",
+            "end_time": f"{self.end_date_input.value}T{self.end_time_input.value}:00",
+            "all_day": self.all_day_checkbox.value,
             "location": self.event_location_input.value,
             "organizer_name": self.user_input.value,
             "organizer_password": self.password_input.value,
             "description": self.description_input.value,
-            "start_time": f"{self.start_date_input.value}T{self.start_time_input.value}:00",
-            "end_time": f"{self.end_date_input.value}T{self.end_time_input.value}:00",
-            "all_day": self.all_day_checkbox.value,
         }
