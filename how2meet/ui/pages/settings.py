62d13991c06f8e1c1433c88caff811d047e82548
csrubin <csrubin@users.noreply.github.com>
2024-02-15 21:04:19 +0000

    linting via pre-commit setup

--- how2meet/ui/pages/settings.py
+++ how2meet/ui/pages/settings.py
@@ -3,6 +3,7 @@ import logging
 from nicegui import APIRouter, app, ui
 
 from how2meet.ui.components import elements
+
 from ..components.frames import frame
 from .urls import URL_SETTINGS
 
@@ -11,10 +12,11 @@ logger.setLevel(logging.DEBUG)
 
 router = APIRouter(prefix=URL_SETTINGS, tags=["settings"])
 
+
 @router.page("/")
 def settings():
     """Settings page"""
     frame("Settings")
-        # NOTE dark mode will be persistent for each user across tabs and server restarts
+    # NOTE dark mode will be persistent for each user across tabs and server restarts
     ui.dark_mode().bind_value(app.storage.user, "dark_mode")
     elements.checkbox("dark mode").bind_value(app.storage.user, "dark_mode")
