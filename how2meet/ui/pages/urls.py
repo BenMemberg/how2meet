25117b41ce51c8e4f0683f572932b3d8938b4306
Taea <88346289+adrastaea@users.noreply.github.com>
2024-02-16 20:35:30 -0500

    test path op change

--- how2meet/ui/pages/urls.py
+++ how2meet/ui/pages/urls.py
@@ -3,7 +3,7 @@ BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
 ROUTE_PREFIX_EVENTS = "/events"
 ROUTE_BASE = "/"
 ROUTE_NEW_EVENT = "/create"
-ROUTE_EVENT_HOME = "/{event_id}"
+ROUTE_EVENT_HOME = "/home/{event_id}"
 
 URL_EVENTS = ROUTE_PREFIX_EVENTS + ROUTE_BASE
 URL_NEW_EVENT = ROUTE_PREFIX_EVENTS + ROUTE_NEW_EVENT
