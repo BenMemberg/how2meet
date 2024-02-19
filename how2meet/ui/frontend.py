f0c4267b099a180184af5022e20eef64dc1d55e2
Taea <88346289+adrastaea@users.noreply.github.com>
2024-02-19 14:23:30 -0500

    rm test code

--- how2meet/ui/frontend.py
+++ how2meet/ui/frontend.py
@@ -24,21 +24,10 @@ def home():
     """Home page"""
     frame()
 
-    # @ui.refreshable
-    # def show_link_submit(link_input):
-    #     if link_input.value:
-    #         elements.button("Go", on_click=partial(ui.open, link_input.value)).classes("mt-4")
-
     with ui.column().classes("w-full absolute-center items-center"):
         elements.label("Welcome to How2Meet").classes("text-3xl font-bold mb-4")
         elements.label("The zero sign up event manager").classes("text-xl mb-4")
         elements.button("New Event", on_click=lambda: ui.open(URL_NEW_EVENT))
-        # with ui.row().classes("w-full justify-center items-center"):
-        #     link_input = elements.input("I have a link...").classes("mt-4")
-        #     link_input.on("keydown.enter", lambda: ui.open(link_input.value))
-        #     show_link_submit(link_input)
-        #     link_input.on("change", partial(show_link_submit.refresh, link_input))
-
 
 def init(fastapi_app) -> None:
     """
